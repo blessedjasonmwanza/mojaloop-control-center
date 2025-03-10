fc4b51da313d284e08131ea261091ca912145aa3
pye-aung <pye.aung@thitsaworks.com>
2025-03-10 21:11:14 +0630

    update alignment

--- movestatefromgitlab.sh
+++ movestatefromgitlab.sh
@@ -1,41 +1,35 @@
-  terragrunt run-all init -upgrade
-
-  cat <<'EOT' >terragrunt.hcl
-  skip = false
-  remote_state {
-    backend = "local"
-    config = {
-      path = "${get_parent_terragrunt_dir()}/${path_relative_to_include()}/terraform.tfstate"
-    }
-
-    generate = {
-      path = "backend.tf"
-      if_exists = "overwrite"
-    }
+terragrunt run-all init -upgrade
+cat <<'EOT' >terragrunt.hcl
+skip = false
+remote_state {
+  backend = "local"
+  config = {
+    path = "${get_parent_terragrunt_dir()}/${path_relative_to_include()}/terraform.tfstate"
   }
+  generate = {
+    path = "backend.tf"
+    if_exists = "overwrite"
+  }
+}
+generate "required_providers" {
+  path = "required_providers.tf"
+  if_exists = "overwrite_terragrunt"
+  contents = <<EOF
+terraform { 
+  required_version = "${local.common_vars.tf_version}"
 
-  generate "required_providers" {
-    path = "required_providers.tf"
-
-    if_exists = "overwrite_terragrunt"
-
-    contents = <<EOF
-  terraform { 
-    required_version = "${local.common_vars.tf_version}"
-  
-    required_providers {
-      local = {
-        source = "hashicorp/local"
-        version = "${local.common_vars.local_provider_version}"
-      }
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "${local.common_vars.local_provider_version}"
     }
   }
-  EOF
-  }
-
-  locals {
-    common_vars = yamldecode(file("common-vars.yaml"))
-    env_vars = yamldecode(file("environment.yaml"))
-  }
-  EOT
-  terragrunt run-all init -migrate-state -force-copy
+}
+EOF
+}
+locals {
+  common_vars = yamldecode(file("common-vars.yaml"))
+  env_vars = yamldecode(file("environment.yaml"))
+}
+EOT
+terragrunt run-all init -migrate-state -force-copy
